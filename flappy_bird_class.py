fa9919b0eb93dcd9e7d7585ab6bf1cae504f649f
davidkorea <lyfirst@naver.com>
2018-07-12 11:31:30 +0900

    utils & class % main

--- flappy_bird_class.py
+++ flappy_bird_class.py
@@ -12,7 +12,7 @@ SCREENWIDTH = 288
 SCREENHEIGHT = 512
 
 pygame.init()
-FPSCLOCK = pygame.time.clock()
+FPSCLOCK = pygame.time.Clock()
 SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption('Flappy Bird')
 
@@ -26,7 +26,7 @@ PIPE_WIDTH = IMAGES['pipe'][0].get_width()
 PIPE_HEIGHT = IMAGES['pipe'][0].get_height()
 BACKGROUND_WIDTH = IMAGES['background'].get_width()
 
-PLAYER_INDEX_GEN = cycle([0,1,2,1])
+PLAYER_INDEX_GEN = cycle([0, 1, 2, 1])
 
 class GameState:
     def __init__(self):
@@ -44,7 +44,7 @@ class GameState:
             {'x': SCREENWIDTH, 'y': newPipe1[1]['y']},
         ]
 
-        self.pipeVelx = -4
+        self.pipeVelX = -4
         self.playerVelY = 0
         self.playerMaxVelY = 10
         self.playerMinVelY = -8
@@ -54,9 +54,98 @@ class GameState:
 
     def frame_step(self, input_actions):
         pygame.event.pump()
+
         terminal = False
 
         if input_actions % 3 == 1:
             if self.playery > -2 * PLAYER_HEIGHT:
                 self.playerVelY = self.playerFlapAcc
                 self.playerFlapped = True
+
+        # player fly by playerindex
+        if (self.loopIter + 1) % 3 ==0:
+            self.playerIndex = next(PLAYER_INDEX_GEN)
+            self.loopIter = (self.loopIter + 1) % 30
+            self.basex = -((-self.basex + 100) % self.baseShift)
+
+        # player's movement
+        if self.playerVelY < self.playerMaxVelY and not self.playerFlapped:
+            self.playerVelY += self.playerAccY
+        if self.playerFlapped:
+            self.playerFlapped = False
+
+        self.playery += min(self.playerVelY, BASEY - self.playery - PLAYER_HEIGHT)
+
+        # 这个需要改动
+        if self.playery < 0:
+            self.playery = 0
+
+        # move pipes to left
+        for uPipe, lPipe in zip(self.upperPipes, self.lowerPipes):
+            uPipe['x'] += self.pipeVelX
+            lPipe['x'] += self.pipeVelX
+
+        # add new pipe when first pipe is about to touch left of screen
+        if 0 < self.upperPipes[0]['x'] < 5:
+            newPipe = getRandomPipe()
+            self.upperPipes.append(newPipe[0])
+            self.lowerPipes.append(newPipe[1])
+
+        # remove first pipe if its out of the screen
+        if self.upperPipes[0]['x'] < -PIPE_WIDTH:
+            self.upperPipes.pop(0)
+            self.lowerPipes.pop(0)
+
+        # draw sprites
+        SCREEN.blit(IMAGES['background'], (0, 0))
+
+        for uPipe, lPipe in zip(self.upperPipes, self.lowerPipes):
+            SCREEN.blit(IMAGES['pipe'][0], (uPipe['x'], uPipe['y']))
+            SCREEN.blit(IMAGES['pipe'][1], (lPipe['x'], lPipe['y']))
+
+        SCREEN.blit(IMAGES['base'], (self.basex, BASEY))
+
+        # print score so player overlaps the score
+        # showScore(self.score)
+        SCREEN.blit(IMAGES['player'][self.playerIndex],
+                    (self.playerx, self.playery))
+
+        image_data = pygame.surfarray.array3d(pygame.display.get_surface())
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+        return image_data
+
+def getRandomPipe():
+    """returns a randomly generated pipe"""
+    # y of gap between upper and lower pipe
+    gapYs = [10, 30, 50, 60, 80, 110, 140, 160, 180, 210]
+    # gapYs = [40]
+    index = random.randint(0, len(gapYs) - 1)
+    gapY = gapYs[index]
+
+    gapY += int(BASEY * 0.2)
+    pipeX = SCREENWIDTH * 1.1 + 10
+
+    return [
+        {'x': pipeX, 'y': gapY - PIPE_HEIGHT},  # upper pipe
+        {'x': pipeX, 'y': gapY + PIPEGAPSIZE},  # lower pipe
+    ]
+
+def showScore(score):
+    """displays score in center of screen"""
+    scoreDigits = [int(x) for x in list(str(score))]
+    totalWidth = 0  # total width of all numbers to be printed
+
+    for digit in scoreDigits:
+        totalWidth += IMAGES['numbers'][digit].get_width()
+
+    Xoffset = (SCREENWIDTH - totalWidth) / 2
+
+    for digit in scoreDigits:
+        SCREEN.blit(IMAGES['numbers'][digit], (Xoffset, SCREENHEIGHT * 0.1))
+        Xoffset += IMAGES['numbers'][digit].get_width()
+
+
+
+
